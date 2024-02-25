6ba90862394ff2f6384fbb8dded117dbb28ad5a5
Bartłomiej Czudek <bartlomiej.czudek@gft.com>
2024-02-25 23:23:24 +0100

    fix

--- demos/demo2/example.sh
+++ demos/demo2/example.sh
@@ -1,4 +1,4 @@
+bpftrace -e 'tracepoint:raw_syscalls:sys_enter { @[comm] = count(); }'
+
 bpftrace -l | wc -l
 bpftrace -l "*tracepoint:*" | wc -l
-
-bpftrace -e 'tracepoint:raw_syscalls:sys_enter { @[comm] = count(); }'
