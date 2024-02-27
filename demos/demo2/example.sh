a8d5201d7f34aa1749219e0020ae1e34a6e76b90
Bartłomiej Czudek <bartlomiej.czudek@gft.com>
2024-02-27 12:43:52 +0100

    added one more example of bpftrace

--- demos/demo2/example.sh
+++ demos/demo2/example.sh
@@ -1,4 +1,6 @@
 bpftrace -e 'tracepoint:raw_syscalls:sys_enter { @[comm] = count(); }'
+bpftrace -e 'tracepoint:syscalls:sys_enter_openat /comm == "cat"/ { printf("%s %s\n", comm, str(args->filename)) }'
 
 bpftrace -l | wc -l
 bpftrace -l "*tracepoint:*" | wc -l
+comm	Process or command name
