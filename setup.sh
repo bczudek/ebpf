fa43fa0b1426edc3f8be3eefd5a04598c9ff809b
Bartłomiej Czudek <bartlomiej.czudek@gft.com>
2024-03-06 08:16:03 +0100

    improved the setup

--- setup.sh
+++ setup.sh
@@ -5,14 +5,15 @@ apt-get -y upgrade
 apt-get install -y make pkg-config libelf-dev gcc
 apt-get install -y bpfcc-tools linux-headers-$(uname -r)
 
-git clone --recurse-submodules https://github.com/lizrice/learning-ebpf
-cd learning-ebpf
-
-cd libbpf/src
+git clone https://github.com/libbpf/libbpf.git
+cd libbpf
+# the specific version is inspired by Liz Rice repository
+git reset --hard a6d7530cb7dff87ac1e64a540e63b67ddde2e0f9
+cd src
 make install 
 cd ../..
 
-cd ..
+#cd ..
 git clone --recurse-submodules https://github.com/libbpf/bpftool.git
 cd bpftool/src 
 make install 
@@ -20,6 +21,8 @@ make install
 wget https://github.com/bpftrace/bpftrace/releases/download/v0.20.1/bpftrace
 mv bpftrace /usr/bin/bpftrace
 chmod +x /usr/bin/bpftrace
+
+cd ../..
 
 apt update
 apt install -y clang
